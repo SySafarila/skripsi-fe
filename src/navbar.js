e20244dcc13372cc3eacae7ddc08d8a807046278
Syahrul Safarila <57931052+SySafarila@users.noreply.github.com>
2024-05-12 21:35:58 +0700

    Update navbar.js
    Update navbar.js

--- src/navbar.js
+++ src/navbar.js
@@ -1,30 +1,36 @@
-const navMenus = document.querySelector("nav #menus");
-const navHamburger = document.querySelector("nav #hamburger");
-const navBackdrop = document.querySelector("nav #backdrop");
+const navbarActions = () => {
+  const navMenus = document.querySelector("nav #menus");
+  const navHamburger = document.querySelector("nav #hamburger");
+  const navBackdrop = document.querySelector("nav #backdrop");
+  const body = document.querySelector("body");
 
-const menusToggle = () => {
-  navMenus.classList.toggle("translate-x-full");
+  const menusToggle = () => {
+    body.classList.toggle("overflow-y-hidden");
+    navMenus.classList.toggle("translate-x-full");
 
-  if (navBackdrop.classList.contains("hidden")) {
-    navBackdrop.classList.toggle("hidden");
-    setTimeout(() => {
-      navBackdrop.classList.toggle("bg-black/50");
-    }, 50);
-  } else {
-    navBackdrop.classList.toggle("bg-black/50");
-    setTimeout(() => {
+    if (navBackdrop.classList.contains("hidden")) {
       navBackdrop.classList.toggle("hidden");
-    }, 150);
-  }
+      setTimeout(() => {
+        navBackdrop.classList.toggle("bg-black/50");
+      }, 50);
+    } else {
+      navBackdrop.classList.toggle("bg-black/50");
+      setTimeout(() => {
+        navBackdrop.classList.toggle("hidden");
+      }, 150);
+    }
+  };
+
+  navHamburger.addEventListener("click", (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    menusToggle();
+  });
+  navBackdrop.addEventListener("click", (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    menusToggle();
+  });
 };
 
-navHamburger.addEventListener("click", (e) => {
-  e.preventDefault();
-  e.stopPropagation();
-  menusToggle();
-});
-navBackdrop.addEventListener("click", (e) => {
-  e.preventDefault();
-  e.stopPropagation();
-  menusToggle();
-});
+navbarActions();
